09b23aadf3b956c6b4b861cff91b3725ea974605
jglez <jglez.cs@gmail.com>
2021-03-30 13:36:44 -0500

    Added catch blocks to use error handling middleware in user-router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -7,15 +7,21 @@ const router = express.Router()
 
 /***** ENDPOINTS *****/
 // [POST] user signup
-router.post('/signup', (req, res) => {
+router.post('/signup', (req, res, next) => {
   User.create()
     .then(res.status(200).json('Inside of signup'))
+    .catch(next) // jump to the error handling middleware at the end of server.js
 })
 
 // [POST] user login
-router.post('/login', (req, res) => {
-  User.findById()
-    .then(res.status(200).json('Inside of login'))
+router.get('/login', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.body)
+    res.status(200).json(user)
+
+  } catch (err) {
+    next(err)
+  }
 })
 
 // export

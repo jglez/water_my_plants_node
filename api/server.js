e1d50e8bbe71bc16320c376ffb828b9e83209326
jglez <jglez.cs@gmail.com>
2021-03-30 12:56:02 -0500

    Added CORS to package deps

--- api/server.js
+++ api/server.js
@@ -1,20 +1,22 @@
 // imports
 const express = require('express')
+const cors = require('cors')
 const usersRouter = require('./users/users-router.js')
-const morgan = require('morgan') // network activity logger
+const morgan = require('morgan')
 
 // instantiate Express application
 const server = express()
 
 // global middleware
+server.use(cors()) // enable cross-origin requests
 server.use(express.json()) // enable Express to parse JSON bodies
-server.use(morgan('dev')) // request logger
+server.use(morgan('dev')) // http request logger
 
 // users router
 server.use('/api/users', usersRouter)
 
 // error handling middleware
-server.use((err, req, res, next) => {
+server.use((err, req, res, next) => { //eslint-disable-line
   res.status(500).json({
     message: err.message, // receives actual error that was thrown
     stack: err.stack // function call stack

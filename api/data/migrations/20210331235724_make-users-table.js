00310820ca265fd11f999cafc6adf24de75d6b87
jglez <jglez.cs@gmail.com>
2021-03-31 21:02:43 -0500

    Fixed typo in make-users-table migration

--- api/data/migrations/20210331235724_make-users-table.js
+++ api/data/migrations/20210331235724_make-users-table.js
@@ -1,12 +1,13 @@
-exports.up = function (knex) {
+exports.up = async (knex) => {
   // createTable() takes two args: table name and a cb function that takes the instantiated table
-  return knex.schema.createTable('Users', table => {
+  await knex.schema.createTable('Users', table => {
     table.increments('user_id') // primary key: unique & notNullable constraint - auto increments
     table.string('user_username', 200).notNullable()
     table.string('user_password', 320).notNullable()
+    table.timestamps(true, false) // timestamp for created_at, no updated_at
   })
 };
 
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('Users')
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('Users')
 };
